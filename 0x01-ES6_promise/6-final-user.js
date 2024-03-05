31bdbab24c5da90185167b0496de455ae90b9e6f
Mohammed Shousha <mshousha29@gmail.com>
2024-03-05 21:08:25 +0200

    6-final-user.js

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,22 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(
+  firstName,
+  lastName,
+  fileName,
+) {
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+  const results = await Promise.allSettled(promises);
+
+  return results.map((result) => {
+    if (result.status === 'fulfilled') {
+      return { status: result.status, value: result.value };
+    } else {
+      return {
+        status: result.status,
+        value: `Error: ${result.reason.message}`,
+      };
+    }
+  });
+}
