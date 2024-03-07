a34e663f00d6aea1245263144c74265761276826
Mohammed Shousha <mshousha29@gmail.com>
2024-03-07 13:47:42 +0200

    3-currency.js

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,37 @@
+export default class Currency {
+  constructor(code, name) {
+    this.validateString(code, 'Code');
+    this.validateString(name, 'Name');
+
+    this._code = code;
+    this._name = name;
+  }
+
+  validateString(value, name) {
+    if (typeof value !== 'string') {
+      throw new TypeError(`${name} must be a string`);
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(name) {
+    this.validateString(name, 'Name');
+    this._name = name;
+  }
+
+  set code(code) {
+    this.validateString(code, 'Code');
+    this._code = code;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
